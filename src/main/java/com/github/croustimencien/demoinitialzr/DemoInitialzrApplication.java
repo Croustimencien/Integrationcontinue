29b0492c681f1ef071d3fdc1aef4cdfa4354120b
Le Page Brewen <brewenlepage@gmail.com>
2021-12-13 10:25:07 +0100

    test

---
+++ src/main/java/com/github/croustimencien/demoinitialzr/DemoInitialzrApplication.java
@@ -0,0 +1,13 @@
+package com.github.croustimencien.demoinitialzr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInitialzrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInitialzrApplication.class, args);
+	}
+
+}
